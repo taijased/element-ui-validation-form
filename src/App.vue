e496b887fd8202ac0afb20e79c0b3bf22e318608
Spiridonov Maxim <sp.maxim@ips-dev.com>
2019-01-09 14:05:35 +0300

    add worked api

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     img(alt="Vue logo", src="./assets/logo.png")
     el-button(@click="show = !show") Открыть форму
     .links
-      el-button(type="primary", icon="el-icon-edit") Board in Trello
-      el-button(type="primary", icon="el-icon-share") GitHub
+      a(href="https://trello.com/b/VrbJ6vRD/%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F-%D0%B4%D0%BE%D1%81%D0%BA%D0%B0-%D0%B4%D0%BB%D1%8F-%D1%84%D0%BE%D1%80%D0%BC%D1%8B-vue", target="_blank") Board in Trello
+      a(href="https://github.com/taijased/element-ui-validation-form", target="_blank") GitHub
     RequestModal(v-if="show", title="Форма отправки в Trello")
 </template>
 
@@ -37,4 +37,6 @@ export default {
   align-items center
   .links
     margin-top 25px
+    a 
+      margin 20px
 </style>
--- src/api/ApiTrello.js
+++ src/api/ApiTrello.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 
-// Example privite board 
-
-// const key = '0456580dsa9fdsan0a932498n32b05c7b';
-// const token = '9153bd91c2c038das81f7823gadsddagh984fsd32badyubg9823418d35c';
-// const API_URL_TRELLO = 'https://api.trello.com/1/cards?key='+ key +'&token=' + token
-
-// P.S key and token test value
-
 // Example public board 
 
-const API_URL_TRELLO = 'https://trello.com/c/4hzuJL8E/1-%D1%8D%D1%82%D0%BE-%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F'
+const key = 'a8baabbb0068e0ea8ccd2b02a84c2c48';
+const token = '1dd44945a26e7aad43a71903a8182252772fdf236ef211f320c4d7a7ed21fe3c';
+const API_URL_TRELLO = 'https://api.trello.com/1/cards?key='+ key +'&token=' + token
 
 let ApiTrello = axios.create({
     baseURL: API_URL_TRELLO,
